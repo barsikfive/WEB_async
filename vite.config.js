594a9f3385c7918a1918de6282da47ffd6a5eb51
Alina Abaieva <alina.abaieva@gmail.com>
2025-06-08 21:26:38 +0300

    Configure Vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/WEB_async/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        timer: resolve(__dirname, '1-timer.html'),
+        snackbar: resolve(__dirname, '2-snackbar.html'),
+        images: resolve(__dirname, '3-images.html'),
+      },
+    },
+  },
 });
